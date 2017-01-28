07b39f32f2caade8a0ed55d6f71d9802269c5c03
Mustafa Erkan Başar <mustafaerkanbasar@gmail.com>
2017-01-28 04:13:33 +0100

    shuffle button added

--- static/play.js
+++ static/play.js
@@ -1,4 +1,6 @@
 
+var playlist=[];
+
 // http://stackoverflow.com/questions/22613903/youtube-api-v3-get-list-of-users-videos
 $.get(
     "https://www.googleapis.com/youtube/v3/playlistItems",{
@@ -7,17 +9,15 @@ $.get(
     playlistId : 'PL4Xo_npkQSb3Lfa1vW-miGBqBSv6T0Ds3',
     key: 'AIzaSyC8hlhRBGWzLQAqpKK1OvHtsU_eg56bais'},
     function(data) {
-        var videoIds = [];
         for (var i = 0; i < data.items.length; i++) {
-            videoIds.push(data.items[i].contentDetails.videoId);
+            playlist.push(data.items[i].contentDetails.videoId);
         }
-
-        initialize(videoIds);
+        randomPick();
     }
 );
 
 
-function initialize(videoIds){
-  var initsong = videoIds[Math.floor(Math.random() * videoIds.length)];
-  document.getElementById('player').src = "https://www.youtube.com/embed/"+initsong+"?controls=1&showinfo=1&rel=1&autoplay=1&loop=1&playlist=" + videoIds.join();
+function randomPick(){
+  var initsong = playlist[Math.floor(Math.random() * playlist.length)];
+  document.getElementById('player').src = "https://www.youtube.com/embed/"+initsong+"?controls=1&showinfo=1&rel=1&autoplay=1&loop=1&playlist=" + playlist.join();
 }
