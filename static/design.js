1ff5927b77fece9c27b08cfa3fd0dd32d41522f2
Mustafa Erkan Başar <mustafaerkanbasar@gmail.com>
2017-01-28 20:41:53 +0100

    playlist completed

--- static/design.js
+++ static/design.js
@@ -1,5 +1,13 @@
 
 $(document).ready( function() {
-    $("#playlist").niceScroll();
+    $("#playlist").niceScroll({styler:"fb",cursorcolor:"#FFF", cursorwidth: '8', cursorborderradius: '10px', spacebarenabled:false,  cursorborder: '', zindex: '1000'});
   }
 );
+
+
+jQuery(function($){
+  	$('#menu-btn').click(function(){
+    		$('.responsive-menu').toggleClass('expand')
+    		$('#menu-btn').toggleClass('is-active')
+  	})
+})
--- static/play.js
+++ static/play.js
@@ -1,27 +1,51 @@
 
+var ytbdata;
 var playlist=[];
 
 // http://stackoverflow.com/questions/22613903/youtube-api-v3-get-list-of-users-videos
 $.get(
     "https://www.googleapis.com/youtube/v3/playlistItems",{
-    part : 'contentDetails',
+    part : 'snippet',
     maxResults : 20,
     playlistId : 'PL4Xo_npkQSb3Lfa1vW-miGBqBSv6T0Ds3',
     key: 'AIzaSyC8hlhRBGWzLQAqpKK1OvHtsU_eg56bais'},
     function(data) {
-        for (var i = 0; i < data.items.length; i++) {
-            playlist.push(data.items[i].contentDetails.videoId);
+        ytbdata = data;
+        console.log(ytbdata);
+        for (var i = 0; i < ytbdata.items.length; i++) {
+            playlist.push(ytbdata.items[i].snippet.resourceId.videoId);
         }
         randomPick();
+        playlistInit();
     }
 );
-
 
 function randomPick(){
   var initsong = playlist[Math.floor(Math.random() * playlist.length)];
   document.getElementById('fullscreenplayer').src = "https://www.youtube.com/embed/"+initsong+"?controls=1&showinfo=1&rel=1&autoplay=1&loop=1&playlist=" + playlist.join();
 }
 
+function playSong(songid){
+  return function(){
+    document.getElementById('fullscreenplayer').src = "https://www.youtube.com/embed/"+songid+"?controls=1&showinfo=1&rel=1&autoplay=1&loop=1&playlist=" + playlist.join();
+  }
+}
 
-function playlistinit(playlist) {
+function playlistInit() {
+
+  for (var i = 0; i < ytbdata.items.length; i++) {
+
+    var videoid = ytbdata.items[i].snippet.resourceId.videoId;
+    var title = ytbdata.items[i].snippet.title;
+
+    if (ytbdata.items[i].snippet.thumbnails.standard) {
+       var thumbnail = ytbdata.items[i].snippet.thumbnails.standard.url;
+    }else {
+       var thumbnail = ytbdata.items[i].snippet.thumbnails.default.url;
+    }
+
+    $("#playlist").append('<div class="hovereffect" id="' + videoid + '"><img class="img-responsive" src="' + thumbnail + '" alt=""><div id="hzi8T2aFMls" class="overlay"><h2>' + title + '</h2><p><a href="#"><i class="fa fa-play"></i></a></p></div></div>');
+
+    $('#'+playlist[i]).click(playSong(playlist[i]));
+  }
 }
